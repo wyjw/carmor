cd7c2e40c17d1e9d98164037ebcfe462013f3869
wyjw <uujiwu@gmail.com>
2019-10-07 01:59:44 -0400

    minor change to a test case

--- outoftree/test_cases/split3.cc
+++ outoftree/test_cases/split3.cc
@@ -9,10 +9,10 @@ int add_one (int x) {
 }
 
 int main () {
-  int a = 4; ///@1
-  a = 6; ///@1
-  a = 45; ///@1
-  int b = 5; ///@2
+  int a = 4; ///@2
+  a = 6; ///@2
+  a = 45; ///@2
+  int b = 5; ///@1
   int x = add_one(a); ///@1
   int y = sub_one(b); ///@2
 }
